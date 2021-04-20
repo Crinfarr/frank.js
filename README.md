7531604cba273fa2024e049e51fc9ec09b42f418
Crinfarr <31548109+Crinfarr@users.noreply.github.com>
2021-04-19 23:43:50 -0500

    add orbit light, readme, and update some textures

---
+++ README.md
@@ -0,0 +1,11 @@
+#FRANK.JS
+###created by Crinfarr/a.Hoeveler 2021
+[![Website hecancanyou.xyz](https://img.shields.io/website-up-down-green-red/http/hecancanyou.xyz.svg)](http://hecancanyou.xyz/frank.js/)
+
+
+Resources used:
+[THREE.js by mrDoob](https://threejs.org/)
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+
+[Solar Textures from SolarSystemScope](https://www.solarsystemscope.com/textures/)
+[![CC-BY license](https://img.shields.io/badge/License-CC--BY-blue.svg)](https://creativecommons.org/licenses/by-nd/4.0)
--- js/frank.js
+++ js/frank.js
@@ -6,13 +6,22 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 var options = {
     grids: {
         rect: false,
-        radi: false
+        radi: true
     },
     lights: {
         sun: true,
         dev: false
     }
 };
+
+function orbit(center, orbiter, distance, height, angle) {
+    let cpos = [center.position.x, center.position.y, center.position.z];
+    let dx = distance * Math.sin(angle);
+    let dy = height;
+    let dz = distance * Math.cos(angle);
+    orbiter.position.set(cpos[0] + dx, cpos[1] + dy, cpos[2] + dz);
+}
+
 //set camera position
 camera.position.set(3, 3, 5);
 
@@ -24,45 +33,71 @@ document.body.appendChild(renderer.domElement);
 
 //create texture loader and eaerth/cloud mats
 let txtloader = new THREE.TextureLoader();
-let earthmat = new THREE.MeshStandardMaterial({
-    color: 0xffffff,//default color
-
-    //textures
-    map: THREE.ImageUtils.loadTexture('res/earth/8k_earth_daymap.jpg'),
-    normalMap: THREE.ImageUtils.loadTexture('res/earth/8k_earth_normal_map.jpg'),
-
-    //roughness maps
-    roughness: 1,
-    roughnessMap: THREE.ImageUtils.loadTexture('res/earth/8k_earth_specular_map.tif'),
-
-    //night time
-    lightMapIntensity: 0.5,
-    emissive: 0xffffff,
-    lightMap: THREE.ImageUtils.loadTexture('res/earth/8k_earth_nightmap.jpg')
-});
-let earthgeo = new THREE.SphereGeometry(2, 30, 30);
-
-let cloudmat = new THREE.MeshStandardMaterial({
-    color: 0xffffff,
-    opacity: 1,
-    roughness: 0.325
-});
-
-let cloudgeo = new THREE.SphereGeometry(2.05, 30, 30);
-txtloader.load('res/earth/clouds/8k_earth_clouds.jpg', (texture) => {
-    //set texture
-    clouds.material.map = texture;
-    clouds.material.needsUpdate = true;
-    console.log('loaded cloud textures and attempted overlay');
-
+function loadMats() {
+    //load textures
+    txtloader.load('res/earth/8k_earth_daymap.jpg', (texture) => {
+        earth.material.map = texture;
+        earth.material.needsUpdate = true;
+        console.log('loaded earth texture');
+    });
+    //load normals
+    txtloader.load('res/earth/8k_earth_normal_map.jpg', (normal) => {
+        earth.material.normalMap = normal;
+        earth.material.needsUpdate = true;
+        console.log('loaded earth normals');
+    });
+    //load rough maps
+    txtloader.load('res/earth/8k_earth_specular_map.jpg', (spec) => {
+        earth.material.roughnessMap = spec;
+        earth.material.roughness = 1;
+        earth.material.needsUpdate = true;
+        console.log('loaded specular roughness map');
+    });
+    //load light maps
+    //(for night time)
+    txtloader.load('res/earth/8k_earth_nightmap.jpg', (lmap) => {
+        earth.material.emissiveMap = lmap;
+        earth.material.emissiveIntensity = 0.6;
+        earth.material.needsUpdate = true;
+        console.log('loaded nightlight map');
+    });
+    //load cloud texture
+    txtloader.load('res/earth/clouds/8k_earth_clouds.jpg', (texture) => {
+        clouds.material.map = texture;
+        clouds.material.needsUpdate = true;
+        console.log('loaded cloud textures and attempted overlay');
+    });
+    //load cloud alpha map
     txtloader.load('res/earth/clouds/8k_earth_clouds.jpg', (alphamap) => {
-        //set alpha map
         clouds.material.alphaMap = alphamap;
         clouds.material.transparent = true;
         clouds.material.needsUpdate = true;
         console.log('loaded cloud transparency and attempted apply');
     });
+    txtloader.load('res/stars/8k_stars_milky_way.jpg', (starmap) => {
+        let rt = new THREE.WebGLCubeRenderTarget(starmap.image.height);
+        rt.fromEquirectangularTexture(renderer, starmap);
+        scene.background = rt.texture;
+        console.log('loaded star textures');
+    });
+}
+loadMats();
+
+//create earth
+let earthmat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    emissive: 0xffffff
 });
+let earthgeo = new THREE.SphereGeometry(2, 30, 30);
+
+//create cloud layer
+let cloudmat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    opacity: 1,
+    roughness: 0.325
+});
+let cloudgeo = new THREE.SphereGeometry(2.05, 30, 30);
+
 
 //create meshes
 earth = new THREE.Mesh(earthgeo, earthmat);
@@ -73,8 +108,8 @@ clouds = new THREE.Mesh(cloudgeo, cloudmat);
 //DISABLE THESE IN PROD
 const grid = new THREE.GridHelper(10, 10);
 const pgrid = new THREE.PolarGridHelper(10, 10, 5, 64);
-// scene.add(pgrid);
-// scene.add(grid);
+if (options.grids.radi) scene.add(pgrid);
+if (options.grids.rect) scene.add(grid);
 
 //add objects to scene
 scene.add(earth);
@@ -84,28 +119,43 @@ scene.add(clouds);
 //DISABLE THIS IN PROD
 // scene.add(new THREE.HemisphereLight(0xffffFF, 0x080820, 1));
 
+//add sun
 let l = new THREE.PointLight(0xfffffb, 1, 0, 20);
-l.position.set(camera.position.x + 2, camera.position.y + 2, -camera.position.z - 2);
+l.position.set(camera.position.x + 2, camera.position.y + 2, camera.position.z + 2);
 scene.add(l);
 
 
 earth.rotation.y = 0.0000041781 * new Date().getTime();
+clouds.rotation.y = 0.0000041781 * new Date().getTime();
 
-let cloudspeed = 0.0005;
+let cloudspeed = 0.00005;
 setInterval(() => {
-    if (Math.round(Math.random()) || cloudspeed >= 0.001) {
-        cloudspeed -= 0.0001;
+    if (Math.round(Math.random()) || cloudspeed >= 0.0001) {
+        cloudspeed -= 0.00001;
         // console.log('decreased cloud speed: ' + Math.round(cloudspeed * 10000) / 10000);
     }
     else {
-        cloudspeed += 0.0001;
+        cloudspeed += 0.00001;
         // console.log('increased cloud speed: ' + Math.round(cloudspeed * 10000) / 10000);
     }
 }, 1000);
 
+let moongeo = new THREE.SphereGeometry(0.5, 20, 20);
+let moonmat = new THREE.MeshBasicMaterial({
+    color: 0xffffff
+});
+let moon = new THREE.Mesh(moongeo, moonmat);
+let moonorbit = 0;
+scene.add(moon);
+moon.position.set(0, 4, 0);
+
 //render animation frames
 function animate(elapsed) {
     requestAnimationFrame(animate);
+    moonorbit += 0.01;
+    orbit(earth, moon, 5, 0, moonorbit);
+
+    l.position.set(moon.position.x, moon.position.y, moon.position.z);
 
     earth.rotation.y += 0.00041781;
     clouds.rotation.y += cloudspeed;
@@ -114,5 +164,4 @@ function animate(elapsed) {
     camera.lookAt(earth.position.x, earth.position.y, earth.position.z);
     renderer.render(scene, camera);
 }
-
 animate();
