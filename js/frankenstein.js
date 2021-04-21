d4b385078e991333cdfe48338de59a0ab0b8f20d
Crinfarr <31548109+Crinfarr@users.noreply.github.com>
2021-04-21 02:33:47 -0500

    fix image sourcing

--- js/frankenstein.js
+++ js/frankenstein.js
@@ -1,6 +1,6 @@
 const frankenstein = new story([
     {
-        avatar: '/src/Crinfarr.png',
+        avatar: 'src/Crinfarr.png',
         location: [0, 2.5, 2],//
         line: `This is an interactive map.
         Although it will work with most settings, I would recommend using 8K textures and putting the storybox on the top bar.<br/><br/>
@@ -12,62 +12,63 @@ const frankenstein = new story([
         //delay:0//don't bother with this
     },
     {
-        avatar: '/src/walton.jpg',
+        avatar: 'src/walton.jpg',
         location: [0.6, 2.3, 1.1],
         line: `I am already far north of London; and as I walk in the streets of Petersburgh, I feel a cold northern breeze play upon my cheeks, which braces my nerves, and fills me with delight.`
     },
     {
-        avatar: '/src/walton.jpg',
+        avatar: 'src/walton.jpg',
         location: [0.8, 2.8, 1.3],
         line: `Do you understand this feeling?<br/>This breeze, which has travelled from the regions towards which I am advancing, gives me a foretaste of those icy chimes.`
     },
     {
-        avatar: '/src/default.png',
+        avatar: 'src/default.png',
         location: [0.6, 3.2, 1],
         line: 'and so he journeyed to the north, in search of adventure.'
     },
     {
-        avatar: '/src/default.png',
+        avatar: 'src/default.png',
         location: [0.325, 3.2, 0.325],
         line: 'That is, until the unyeilding ice did exactly what he was told it would.'
     },
     {
-        avatar: '/src/walton.jpg',
+        avatar: 'src/walton.jpg',
         location: [0.325, 3.2, 0.325],
         line: "uh oh"
     },
     {
-        avatar: '/src/walton.jpg',
+        avatar: 'src/walton.jpg',
         location: [0.2, 3.2, 0.2],
         line: `<p style='font-family:monospace;'>LETTER DATE: AUGUST 15, 17XX.</p>`
     },
     {
-        avatar: '/src/walton.jpg',
+        avatar: 'src/walton.jpg',
         line: `So strange an accident has happened to us that I cannot forbear recording it, although it is very probable you will see me before these papers can come into your posession.`
     },
     {
-        avatar: '/src/walton.jpg',
+        avatar: 'src/walton.jpg',
         line: `In the morning, as soon as it was light, I went upon deck, and found all the sailors busy on one side of the vessel, apparently talking to some in the sea.`
     },
     {
-        avatar: `/src/walton.jpg`,
+        avatar: `src/walton.jpg`,
         line: `On perceiving me, the stranger addressed me in English, although with a foreign accent.`
     },
     {
-        avatar: '/src/drfrank.jpg',
+        avatar: 'src/drfrank.jpg',
         line: 'Before I come aboard your vessel, will you have the kindness to inform me whither you are bound?'
     },
     {
+        location: [0.2, 3.2, 0.2],
         line: 'This stranger is Dr. Victor Frankenstein, and to get to this point we must first rewind several hundred miles.'
     },
     {
         location: [0.1, 2, 2],
-        line: 'Victor grew up part of a distinguished family in Geneva, with his '
+        line: 'Victor grew up part of a distinguished family in Geneva, an only child save his "more than sister."'
     }
 
 ]);
 
-function showdialog(imgID, boxID, text, imgsrc = '/src/default.png') {
+function showdialog(imgID, boxID, text, imgsrc = 'src/default.png') {
     document.getElementById(imgID).src = imgsrc;
     document.getElementById(boxID).innerHTML = text.toString();
 }
