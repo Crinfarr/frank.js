11efd7f7ac48c1090ed1ae5f05c219e5c14cddd5
Crinfarr <31548109+Crinfarr@users.noreply.github.com>
2021-04-25 16:57:06 -0500

    add chapter 2 and make avatar system generic

--- js/frankenstein.js
+++ js/frankenstein.js
@@ -1,7 +1,7 @@
 const frankenstein = new story([
     {
-        avatar: 'src/Crinfarr.png',
-        location: [0, 2.5, 2],//
+        avatar: 'Crinfarr',
+        location: [0, 2.5, 2],
         line: `This is an interactive map.
         Although it will work with most settings, I would recommend using 8K textures and putting the storybox on the top bar.<br/><br/>
         This engine is also open source on <a href='http://github.com/Crinfarr/Frank.js'>My git,</a> and runs uncompiled on web.<p>\n\n\n
@@ -9,52 +9,49 @@ const frankenstein = new story([
         p.s. arrow keys left and right are also supported for navigating the story.<p>\n</p>
         p.p.s at least try it, I spent like 20 minutes making it work right.<p>\n</p>
         p.p.p.s all images taken from the first page of google search results because I am lazy.`
-        //delay:0//don't bother with this
     },
     {
-        avatar: 'src/walton.jpg',
+        avatar: 'walton',
         location: [0.6, 2.3, 1.1],
         line: `I am already far north of London; and as I walk in the streets of Petersburgh, I feel a cold northern breeze play upon my cheeks, which braces my nerves, and fills me with delight.`
     },
     {
-        avatar: 'src/walton.jpg',
+        avatar: 'walton',
         location: [0.8, 2.8, 1.3],
         line: `Do you understand this feeling?<br/>This breeze, which has travelled from the regions towards which I am advancing, gives me a foretaste of those icy chimes.`
     },
     {
-        avatar: 'src/default.png',
         location: [0.6, 3.2, 1],
         line: 'and so he journeyed to the north, in search of adventure.'
     },
     {
-        avatar: 'src/default.png',
         location: [0.325, 3.2, 0.325],
         line: 'That is, until the unyeilding ice did exactly what he was told it would.'
     },
     {
-        avatar: 'src/walton.jpg',
+        avatar: 'walton',
         location: [0.325, 3.2, 0.325],
         line: "uh oh"
     },
     {
-        avatar: 'src/walton.jpg',
+        avatar: 'walton',
         location: [0.2, 3.2, 0.2],
         line: `<p style='font-family:monospace;'>LETTER DATE: AUGUST 15, 17XX.</p>`
     },
     {
-        avatar: 'src/walton.jpg',
+        avatar: 'walton',
         line: `So strange an accident has happened to us that I cannot forbear recording it, although it is very probable you will see me before these papers can come into your posession.`
     },
     {
-        avatar: 'src/walton.jpg',
+        avatar: 'walton',
         line: `In the morning, as soon as it was light, I went upon deck, and found all the sailors busy on one side of the vessel, apparently talking to some in the sea.`
     },
     {
-        avatar: `src/walton.jpg`,
+        avatar: `walton`,
         line: `On perceiving me, the stranger addressed me in English, although with a foreign accent.`
     },
     {
-        avatar: 'src/drfrank.jpg',
+        avatar: 'drfrank',
         line: 'Before I come aboard your vessel, will you have the kindness to inform me whither you are bound?'
     },
     {
@@ -75,7 +72,38 @@ const frankenstein = new story([
         line: "More of a pet than a child."
     },
     {
+        location: [0.1, 2, 2],
+        avatar: 'drfrank',
+        line: 'On the birth of a second son, my junior by seven years, my parents gave up entirely their wandering life and fixed themselves a home in their native country.'
+    },
+    {
+        avatar: 'drfrank',
+        line: 'No human being could have passed a happier childhood than myself. My parents were possessed by the very spirit of kindness and indulgence. We felt that they were not the tyrants to rule our lot according to their caprice, but the agents and creators of all the many delights which we enjoyed.'
+    },
+    {
+        avatar: 'drfrank',
+        line: 'When I was about fifteen years old we had retired to our house near Belrive, when we witnessed a most violent and terrible thunderstorm. It advanced from behind the mountains of Jura; and the thunder burst at once with frightful loudness from various quarters of the heavens. I remained, while the storm lasted, watching its progress with curiosity and delight.'
+    },
+    {
+        avatar: 'hooded',
+        line: "Don't you see?? This proves conclusively my theory of the relation between electricity of galvanism!"
+    },
+    {
+        line: "this fascinated young victor, who would hold the memories of electricity and lightning in his mind for years to come."
+    },
+    {
+        avatar: 'drfrank',
+        line: "When I had attained the age of seventeen, my parents resolved that I should become a student at the university of Ingolstadt."
+    },
+    {
+        line: "(a major university at the time.)"
+    },
+    {
 
     }
 
 ]);
+
+//TODO:
+//  * add locations
+//  * future: add aging profiles?
